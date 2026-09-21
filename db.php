a2f73f6e426aba470b9d456193eb2f1f7661d086
REGASA <regasabedane76@gmail.com>
2026-09-21 09:45:34 +0300

    databse connection occured

---
+++ db.php
@@ -0,0 +1,14 @@
+
+<?php
+$host = "localhost";
+$db   = "stud_management"; // Maqaa database kee isa haaraa
+$user = "root";
+$pass = "";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
